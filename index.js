2df084a33f3467bab2cd7401d1385b2b842b81a8
Amy <llamanozz@gmail.com>
2022-04-10 19:44:21 +0200

    Add query parameters for min and max length

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// start at 1123 of most common words
 require("dotenv").config();
 const fs = require("fs")
 const path = require("path")
@@ -108,36 +109,45 @@ app.get("/api/wordsbyposition", (req, res) => {
 });
 
 app.get("/api/wordsbylength", (req, res) => {
-  res.json({
+  const { minLength, maxLength } = req.query;
+
+  const apiBase = {
     "title": "Most common English words by Wikipedia frequency",
     "description": "This API provides the most common English words used in Wikipedia articles, using the results generated from https://github.com/IlyaSemenov/wikipedia-word-frequency. Proper nouns, place names, non-English words, single-letter words, political and inappropriate words have been removed.",
-    "words_by_length": {
-      "2-5": {
-        "2_letters": getByLength(2),
-        "3_letters": getByLength(3),
-        "4_letters": getByLength(4),
-        "5_letters": getByLength(5)
-      },
-      "6-9": {
-        "6_letters": getByLength(6),
-        "7_letters": getByLength(7),
-        "8_letters": getByLength(8),
-        "9_letters": getByLength(9),
-      },
-      "10-14": {
-        "10_letters": getByLength(10),
-        "11_letters": getByLength(11),
-        "12_letters": getByLength(12),
-        "13_letters": getByLength(13),
-        "14_letters": getByLength(14)
-      }
-    },
     "license": {
       "name": "MIT License",
       "url": "https://choosealicense.com/licenses/mit/"
     },
     "version": "1.0.1"
-  })
+  };
+
+  const minMaxWords = {};
+
+  const getWordsByLength = (min = 2, max = 14) => {
+    // Convert string values of min and max to numbers
+    min = Number(min);
+    max = Number(max);
+
+    // If min is greater than max, swap values
+    if (min > max) {
+      [min, max] = [max, min];
+    }
+
+    // if min and max are not between 2 and 14, set them to 2 and 14, respectively
+    min = (min >= 2 && min <= 14) ? min : 2;
+    max = (min >= 2 && max <= 14) ? max : 14;
+
+    // add the values for each word length to the minMaxWords object
+    for (let i = min; i <= max; i++) {
+      minMaxWords[i + "_letters"] = getByLength(i);
+    }
+
+  }
+  getWordsByLength(minLength, maxLength);
+
+  apiBase["words_by_length"] = minMaxWords;
+
+  res.json(apiBase)
 });
 
 app.listen(port);
