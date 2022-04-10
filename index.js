01a9c0e915940bc98551ebd4d79bd20e1bb18369
Amy <llamanozz@gmail.com>
2022-04-10 18:31:52 +0200

    Separate word length into arrays of 100 words

--- index.js
+++ index.js
@@ -22,9 +22,21 @@ const getByPosition = (start, end) => {
 
 // Return words in order of length
 const getByLength = numLetters => {
-  return allWords.filter(word => word.length === numLetters);
-}
+  const wordsByLength = allWords.filter(word => word.length === numLetters);
+  const hundreds = Math.ceil(wordsByLength.length / 100);
+  const sortedWordsByLength = {};
+  for (let i = 1; i < hundreds + 1; i++) {
+    const currentHundred = [];
+    for (let j = 0; j < 100; j++) {
+      if (wordsByLength[j]) currentHundred.push(wordsByLength[j])
+    }
 
+    sortedWordsByLength[Object.keys(sortedWordsByLength).length * 100 + "-" +  (100 * i - 1)] = currentHundred;
+    wordsByLength.splice(0, 100);
+  }
+
+  return sortedWordsByLength;
+}
 
 app.get("/api/words", (req, res) => {
   res.json({
@@ -71,9 +83,61 @@ app.get("/api/words", (req, res) => {
   })
 });
 
-app.listen(
-  port,
-  () => console.log(`Server running on port ${port}`)
-);
+app.get("/api/wordsbyposition", (req, res) => {
+  res.json({
+    "title": "Most common English words by Wikipedia frequency",
+    "description": "This API provides the most common English words used in Wikipedia articles, using the results generated from https://github.com/IlyaSemenov/wikipedia-word-frequency. Proper nouns, place names, non-English words, single-letter words, political and inappropriate words have been removed.",
+    "words_by_position": {
+      "0-99": getByPosition(0, 99),
+      "100-199": getByPosition(100, 199),
+      "200-299": getByPosition(200, 299),
+      "300-399": getByPosition(300, 399),
+      "400-499": getByPosition(400, 499),
+      "500-599": getByPosition(500, 599),
+      "600-699": getByPosition(600, 699),
+      "700-799": getByPosition(700, 799),
+      "800-899": getByPosition(800, 899),
+      "900-999": getByPosition(900, 999)
+    },
+    "license": {
+      "name": "MIT License",
+      "url": "https://choosealicense.com/licenses/mit/"
+    },
+    "version": "1.0.1"
+  })
+});
 
-// lettersMin and lettersMax endpoints?
+app.get("/api/wordsbylength", (req, res) => {
+  res.json({
+    "title": "Most common English words by Wikipedia frequency",
+    "description": "This API provides the most common English words used in Wikipedia articles, using the results generated from https://github.com/IlyaSemenov/wikipedia-word-frequency. Proper nouns, place names, non-English words, single-letter words, political and inappropriate words have been removed.",
+    "words_by_length": {
+      "2-5": {
+        "2_letters": getByLength(2),
+        "3_letters": getByLength(3),
+        "4_letters": getByLength(4),
+        "5_letters": getByLength(5)
+      },
+      "6-9": {
+        "6_letters": getByLength(6),
+        "7_letters": getByLength(7),
+        "8_letters": getByLength(8),
+        "9_letters": getByLength(9),
+      },
+      "10-14": {
+        "10_letters": getByLength(10),
+        "11_letters": getByLength(11),
+        "12_letters": getByLength(12),
+        "13_letters": getByLength(13),
+        "14_letters": getByLength(14)
+      }
+    },
+    "license": {
+      "name": "MIT License",
+      "url": "https://choosealicense.com/licenses/mit/"
+    },
+    "version": "1.0.1"
+  })
+});
+
+app.listen(port);
